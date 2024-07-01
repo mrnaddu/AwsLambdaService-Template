5143dea30d4c00a4290f7f5cd2a6009d1e8695aa
Mohammed Nadeem <mohammadnadm5@gmail.com>
2024-07-01 19:09:22 +0530

    Exeptions added

---
+++ src/src/AwsLambda.Core/Exceptions/LogicException.cs
@@ -0,0 +1,36 @@
+﻿using AwsLambda.Core.Shared.ErrorCodes;
+
+namespace AwsLambda.Core.Exceptions;
+
+public class LogicException : AppException
+{
+    public LogicException()
+        : base(ApiResultStatusCode.LogicError)
+    {
+    }
+
+    public LogicException(string message)
+        : base(ApiResultStatusCode.LogicError, message)
+    {
+    }
+
+    public LogicException(object additionalData)
+        : base(ApiResultStatusCode.LogicError, additionalData)
+    {
+    }
+
+    public LogicException(string message, object additionalData)
+        : base(ApiResultStatusCode.LogicError, message, additionalData)
+    {
+    }
+
+    public LogicException(string message, Exception exception)
+        : base(ApiResultStatusCode.LogicError, message, exception)
+    {
+    }
+
+    public LogicException(string message, Exception exception, object additionalData)
+        : base(ApiResultStatusCode.LogicError, message, exception, additionalData)
+    {
+    }
+}

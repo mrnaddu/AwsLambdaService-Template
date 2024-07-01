6068ef60e53a75d1c91a2261e66dc8af807cfb96
Mohammed Nadeem <mohammadnadm5@gmail.com>
2024-07-01 10:06:50 +0530

    updated the latest

--- src/src/AwsLambda.Core/Const/AwsConst.cs
+++ src/src/AwsLambda.Core/Const/AwsConst.cs
@@ -5,7 +5,7 @@ public static class AwsConst
     public const string Region = "us-east-1";// eg. us-east-1
     public const string RdsSecretName = "rds-db-credentials"; // eg. rds-db-credentials
     public const string Version = "AWSCURRENT"; // eg. AWSCURRENT
-    public const string DocSecretName = "docdb-db-credentials"; // eg. docdb-db-credentials
+    public const string DocSecretName = "docdb-Credentials"; // eg. docdb-Credentials
     public const string Admin = "admin"; // eg. admin
-    public const string Mechanism = "mechanism"; // eg. mechanism
+    public const string Mechanism = "SCRAM-SHA-1"; // eg. mechanism
 }

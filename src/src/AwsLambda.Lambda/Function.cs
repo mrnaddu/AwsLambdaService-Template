abc44e1c9e1b139de0bf5a591fc052a4f67a6898
Mohammed Nadeem <mohammadnadm5@gmail.com>
2024-07-05 16:27:39 +0530

    updated the document db

--- src/src/AwsLambda.Lambda/Function.cs
+++ src/src/AwsLambda.Lambda/Function.cs
@@ -2,6 +2,7 @@ using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Core;
 using AwsLambda.Application.Contracts.Dtos;
 using AwsLambda.Application.Contracts.ServiceInterfaces;
+using MongoDB.Bson;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,10 +12,12 @@ namespace AwsLambda.Lambda;
 public class Functions
 {
     private readonly ISampleAppService sampleAppService;
+    private readonly IUserAppService userAppService;
 
-    public Functions(ISampleAppService sampleAppService)
+    public Functions(ISampleAppService sampleAppService, IUserAppService userAppService)
     {
         this.sampleAppService = sampleAppService;
+        this.userAppService = userAppService;
     }
 
     // RdsDb MysqlDb
@@ -34,10 +37,10 @@ public class Functions
 
     // DocDb DynamoDb
     [LambdaFunction]
-    public async Task<SampleDto> CreateSampleAsyncHandler(CreateUpdateSampleDto input, ILambdaContext context)
+    public async Task<IEnumerable<BsonDocument>> GetAllUserAsyncHandler(ILambdaContext context)
     {
-        context.Logger.LogDebug($"Received the request with Sample to create new one: {input}");
-        return await sampleAppService.CreateSampleAsync(input);
+        context.Logger.LogDebug($"Received the request for get all users");
+        return await userAppService.GetAllAsync();
     }
 }
 

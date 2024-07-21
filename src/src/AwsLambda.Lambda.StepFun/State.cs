f6b863e7fe8276410080d7daa30578ead0795ad3
Mohammed Nadeem <mohammadnadm5@gmail.com>
2024-07-21 19:47:17 +0530

    updated the changes

--- src/src/AwsLambda.Lambda.StepFun/State.cs
+++
@@ -1,9 +0,0 @@
-namespace AwsLambda.Lambda.StepFun;
-
-
-public class State
-{
-    public string? Name { get; set; }
-    public string? Message { get; set; }
-    public int WaitInSeconds { get; set; }
-}
